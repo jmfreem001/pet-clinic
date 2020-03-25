410dff707201c996759d88c5cc413cf0adbe1762
jmfreem001 <jmfreem001@gmail.com>
2020-03-24 20:22:34 -0500

    Initial Project Structure

---
+++ src/test/java/com/freemanj/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemanj.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
